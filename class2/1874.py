15127fb638527ae7e2f3972840096b2014535162
Kongbee <rhdmsqls5@gmail.com>
2021-12-17 01:15:38 +0900

    1874: 스택 수열

---
+++ class2/1874.py
@@ -0,0 +1,36 @@
+n = int(input())
+input_list = []
+comp_list = [0]
+count_num = 0
+result_list = []
+is_error = False
+
+for i in range(n) :
+    input_list.append(int(input()))
+
+for i in range(n) :
+
+    while input_list[i] != comp_list[-1] :
+
+        if input_list[i] > comp_list[-1] :
+            comp_list.append(count_num)
+            comp_list[-1] += 1
+            count_num = max(count_num, comp_list[-1])
+            result_list.append("+")     
+        
+        elif input_list[i] < comp_list[-1]:
+            is_error = True
+            break
+
+    if is_error:
+        break
+
+    comp_list.pop()
+    result_list.append("-")
+
+if is_error == True :
+    print("NO")
+
+else :
+    for i in result_list:
+        print(i)
