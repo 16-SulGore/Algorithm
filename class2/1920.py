182a09fa919723878b83269e8c70a1d1e5de249b
Kongbee <rhdmsqls5@gmail.com>
2021-12-17 20:51:16 +0900

    1920 refactor: 띄어쓰기 수정

--- class2/1920.py
+++ class2/1920.py
@@ -1,12 +1,12 @@
 n = int(input())
-n_list = list(map(int,input().split()))
+n_list = list(map(int, input().split()))
 
 m = int(input())
-m_list = list(map(int,input().split()))
+m_list = list(map(int, input().split()))
 
 
-for m_num in m_list :
-    if m_num in n_list : 
+for m_num in m_list:
+    if m_num in n_list: 
         print("1")
-    else :
+    else:
         print("0")
