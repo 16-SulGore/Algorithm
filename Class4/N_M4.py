a89f9a17481ab42ccdf236fe80669bbd21431037
jihun-24k <gi020477@gmail.com>
2021-12-23 14:35:39 +0900

    Issue #1 15652 & 15657: 개행 추가

--- Class4/N_M4.py
+++ Class4/N_M4.py
@@ -5,8 +5,10 @@ def dfs(num):
     if len(arr) == M:
         print(" ".join(map(str, arr)))
         return
+
     for i in range(num, N + 1):
         arr.append(i)
         dfs(i)
-        arr.pop()         
+        arr.pop()       
+          
 dfs(1)
--- Class4/N_M8.py
+++ Class4/N_M8.py
@@ -6,8 +6,10 @@ def dfs(num):
     if len(result) == M:
         print(" ".join(map(str, result)))
         return
+
     for i in range(num, N):
         result.append(arr[i])
         dfs(i)
         result.pop()         
+        
 dfs(0)
