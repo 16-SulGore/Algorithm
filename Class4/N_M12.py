b3af41e13dacaa9726eb66cddc12b54acdec2b40
jihun-24k <gi020477@gmail.com>
2021-12-23 14:09:40 +0900

    Issue #1: 15666: 개행추가

--- Class4/N_M12.py
+++ Class4/N_M12.py
@@ -6,8 +6,10 @@ def dfs(num):
     if len(result) == M:
         print(" ".join(map(str, result)))
         return
+
     for i in range(num, len(arr)):
         result.append(arr[i])
         dfs(i)
-        result.pop()         
+        result.pop() 
+                
 dfs(0)
