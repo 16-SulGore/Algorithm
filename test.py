d23d28787b1b0da0efffaf818a2c652f45b65461
차지원 <chajiwon100785@gmail.com>
2021-12-28 03:13:37 +0900

    Issue #13 5639 test: 이전 test skip & 5639번 test 작성

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import unittest
 # python3 -m unnittest test.py
 
 class Test(unittest.TestCase):
+    @unittest.skip("solved")
     def test_1918(self):
         from class4.ex_1918 import solution
 
@@ -11,3 +12,11 @@ class Test(unittest.TestCase):
         self.assertEqual( "AB+", solution("A+B"))
         self.assertEqual("ABC*+", solution("A+B*C"))
         self.assertEqual("ABC*+DE/-", solution("A+B*C-D/E"))
+        
+    def test_5639(self):
+        from class4.ex_5639 import solution
+        node_list = [50, 30, 24, 5, 28, 45, 98, 52, 60]
+        result_list = [5, 28, 24, 45, 30, 60, 52, 98, 50]
+        
+        self.assertEqual(result_list, solution(node_list))
+        
