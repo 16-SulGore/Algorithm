e6e9f8537497165ecadecd2d4af90929c05e3c65
jihun-24k <gi020477@gmail.com>
2021-12-29 20:29:39 +0900

    Issue #13 12865: 평범한 배낭 시간초과

---
+++ Class4/bag.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(num, bag, value):
+    global result
+    result = max(value, result)
+
+    for i in range(num, N):
+        w, v = things[i]
+        if K >= bag + w:
+            dfs(i, bag + w, value + v)
+
+N, K = map(int, input().split())
+
+things = []
+for _ in range(N):
+    weight, value = map(int, input().split())
+    things.append([weight, value])
+
+result = 0
+dfs(0,0,0)
+
+print(result)
