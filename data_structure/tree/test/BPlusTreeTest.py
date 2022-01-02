6cc32a7519cb5e7e1d813ec16bfda3217b9348e6
차지원 <chajiwon100785@gmail.com>
2022-01-03 07:05:00 +0900

    Issue #16 test: insert & linear search test code 작성

---
+++ data_structure/tree/test/BPlusTreeTest.py
@@ -0,0 +1,31 @@
+import unittest
+from ..model.BPlusTree import BPlusTree
+from ..model.Node import Node
+
+
+class BPlusTreeTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        # Given B+Tree & sample keys
+        self.tree = BPlusTree()
+        self.keys = [1, 3, 5, 10, 12, 13, 16, 17, 18]
+        self.sample_keys = [5, 13, 16, 17]
+        # And sample nodes
+        for key in self.sample_keys:
+            self.tree.insert_node(Node(key))
+
+        return super().setUp()
+
+    def testInsertNode_whenInsertNew_thenLinearIsSorted(self):
+        # Given node 15
+        # TODO: mocking node, key
+        new_node = Node(15)
+        self.tree.insert_node(new_node)
+        
+        # When linear search
+        linear = self.tree.search_linear()
+        
+        # Then 15 between 13 & 16
+        # [5, 13, 16, 17] -> [5, 13, 15, 16, 17]
+        expect = sorted(self.sample_keys + new_node.key)
+        self.assertEqual(expect, linear)
