6289f8c075614b1f0d27894367ba4727e4fda050
차지원 <chajiwon100785@gmail.com>
2022-01-04 16:39:34 +0900

    Issue #16 fix: node add 방식 변경, values속성 추가

--- data_structure/tree/model/Node.py
+++ data_structure/tree/model/Node.py
@@ -1,12 +1,39 @@
 class Node:
-    def __init__(self, key) -> None:
-        self.key = [key]
+    def __init__(self) -> None:
+        self.keys = []
+        self.values = []
         self.is_leaf = False
         self.prev = None
         self.next = None
+        self.MAX_ORDER = 2
 
-    def add_key(self, key) -> None:
-        if type(key) is list:
-            self.key += key
+    # https://gist.github.com/savarin/69acd246302567395f65ad6b97ee503d
+    def add(self, key, value):
+        # insert last
+        if not self.keys or key > max(self.keys):
+            self.keys.append(key)
+            self.values.append([value])
+            return
+        
+        # new key
+        for i, old_key in enumerate(self.keys):
+            # existed key
+            if key == old_key:
+                self.values[i].append(value)
+                break
+            
+            # small key
+            # insert left
+            elif key < old_key:
+                self.keys = self.keys[:i] + [key] + self.keys[i:]
+                self.values = self.values[:i] + [[value]] + self.values[i:]
+                break 
+
+    def __eq__(self, __o: object) -> bool:
+        if type(__o) is Node:
+            return self.keys == __o.keys
         else:
-            self.key.append(key)
+            return False
+    
+    def __hash__(self) -> int:
+        return sum(int(self.keys))
