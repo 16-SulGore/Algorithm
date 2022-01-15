c3dd6f2d1932a4a45b698911483b5e41affd416f
차지원 <chajiwon100785@gmail.com>
2022-01-15 20:41:36 +0900

    Issue #20 test: 신규 아이디 추천 입출력 예제 작성

--- programmers/level1/kakao/test.py
+++ programmers/level1/kakao/test.py
@@ -1,8 +1,8 @@
-import re
 import unittest
 
 class Test(unittest.TestCase):
     
+    @unittest.skip("solved")
     def test_신고_결과_받기_01(self):
         from .ex_01 import solution
         
@@ -14,6 +14,7 @@ class Test(unittest.TestCase):
         
         self.assertEqual(result, solution(id_list, report, k))
     
+    @unittest.skip("solved")
     def test_신고_결과_받기_02(self):
         from .ex_01 import solution
         
@@ -24,3 +25,49 @@ class Test(unittest.TestCase):
         result = [0, 0]        
 
         self.assertEqual(result, solution(id_list, report, k))
+
+    def test_신규_아이디_추천_01(self):
+        from .ex_02 import solution
+        
+        new_id = "...!@BaT#*..y.abcdefghijklm"
+        
+        result = "bat.y.abcdefghi"
+        
+        self.assertEqual(result, new_id)
+
+    def test_신규_아이디_추천_02(self):
+        from .ex_02 import solution
+        
+        new_id = "z-+.^."
+        
+        result = "z--"
+        
+        self.assertEqual(result, new_id)
+
+    def test_신규_아이디_추천_03(self):
+        from .ex_02 import solution
+        
+        new_id = "=.="
+        
+        result = "aaa"
+        
+        self.assertEqual(result, new_id)
+        
+        
+    def test_신규_아이디_추천_04(self):
+        from .ex_02 import solution
+        
+        new_id = "123_.def"
+        
+        result = "123_.def"
+        
+        self.assertEqual(result, new_id)
+        
+    def test_신규_아이디_추천_05(self):
+        from .ex_02 import solution
+        
+        new_id = "abcdefghijklmn.p"
+        
+        result = "abcdefghijklmn"
+        
+        self.assertEqual(result, new_id)
