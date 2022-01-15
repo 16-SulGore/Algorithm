8a751ae7377efab139bbf128d93183e8dd45d1ec
차지원 <chajiwon100785@gmail.com>
2022-01-15 19:17:08 +0900

    Issue #20 test: 입출력 예제 작성

---
+++ programmers/level1/kakao/blind2022/test.py
@@ -0,0 +1,26 @@
+import re
+import unittest
+
+class Test(unittest.TestCase):
+    
+    def test_신고_결과_받기_01(self):
+        from .ex_01 import solution
+        
+        id_list = ["muzi", "frodo", "apeach", "neo"]
+        report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+        k = 2
+        
+        result = [2, 1, 1, 0]
+        
+        self.assertEqual(result, solution(id_list, report, k))
+    
+    def test_신고_결과_받기_01(self):
+        from .ex_01 import solution
+        
+        id_list =["con", "ryan"]
+        report = ["ryan con", "ryan con", "ryan con", "ryan con"]
+        k = 3
+        
+        result = [0, 0]        
+
+        self.assertEqual(result, solution(id_list, report, k))
