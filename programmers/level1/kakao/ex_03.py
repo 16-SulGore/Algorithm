16273e2798188b9d23991fe0618b24fce66a17f6
차지원 <chajiwon100785@gmail.com>
2022-01-16 10:20:08 +0900

    Issue #22 fix: dir path

--- programmers/level1/kakao/ex_03.py
+++
@@ -1,4 +0,0 @@
-# 문자열 내림차순 배치하기
-
-def solution(s):
-    return ''.join(sorted(s))[::-1]
--- programmers/level1/kakao/ex_04.py
+++
@@ -1,4 +0,0 @@
-# 문자열 내 p와 y의 개수 비교하기
-
-def solution(s):
-    return s.count('p') + s.count('P') == s.count('y') + s.count('Y')
