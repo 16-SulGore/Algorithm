d9c983aa382a6c76c20adf5c76725de3077c090d
dohun <dohun31@naver.com>
2022-04-23 23:59:57 +0900

    Issue #41 solved: 압축.py

---
+++ week_02/압축.py
@@ -0,0 +1,16 @@
+def solution(msg):
+    start = end = 0
+    msg_length = len(msg)
+    msg_dict = {chr(65 + i): i + 1 for i in range(26)}
+
+    answer = []
+    while start < msg_length:
+        while end < msg_length and msg_dict.get(msg[start:end + 1], 0):
+            end += 1
+        answer.append(msg_dict[msg[start:end]])
+        new_word = msg[start:end + 1]
+        msg_dict[new_word] = len(msg_dict) + 1
+        start = end
+    return answer
+
+print(solution("KAKAO"))
