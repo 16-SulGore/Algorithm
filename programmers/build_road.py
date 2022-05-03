40c7f64dc9e16df9e494df6172e4518ba22929b2
jihun-24k <gi020477@gmail.com>
2022-05-03 16:33:06 +0900

    Issue #65 solved: 경주로 건설

---
+++ programmers/build_road.py
@@ -0,0 +1,34 @@
+# 카카오 기출: 경주로 건설
+from collections import deque
+
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
+NWSE = ['n','w','s','e']
+
+def solution(board):
+    answer = -1
+    n = len(board)
+    q = deque()
+    visited = [[-1 for _ in range(n)]for _ in range(n)]
+    q.append([0,0,'.',0])
+    while q:
+        x, y, d, c = q.popleft() # d = direction, c = cost
+        
+        if (x,y) == (n-1,n-1) and (answer == -1 or answer > c):
+            answer = c
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if board[nx][ny]:
+                continue
+            cost = c + (100 if d == '.' or d == NWSE[i] else 600)
+            if visited[nx][ny] != -1 and visited[nx][ny] < cost - 300:
+                continue
+            q.append([nx,ny,NWSE[i],cost])
+            visited[nx][ny] = cost
+
+    return answer
