91a32da9a839c4e8ec529065227fa5115d6340f7
jihun-24k <gi020477@gmail.com>
2022-10-14 22:17:31 +0900

    Solve: 가장 긴 펠린드롬

--- leetcode/string/longest-palindromic-substring.py
+++ leetcode/string/longest-palindromic-substring.py
@@ -1,5 +1,18 @@
 class Solution:
-    def expand():
-
     def longestPalindrome(self, s: str) -> str:
         
+
+        def expand(left, right):
+            while left >=0 and right <= len(s) and s[left] == s[right - 1]:
+                left -= 1
+                right += 1
+            return s[left + 1: right - 1]
+
+        if len(s) < 2 or s == s[::-1]:
+            return s
+        
+        result = ''
+        for i in range(len(s)-1):
+            result = max(result, expand(i,i+1), expand(i,i+2), key= len)
+
+        return result
